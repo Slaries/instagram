79c3c2fa4091e3eb9a54e0d9b9723f9fd06eda04
Slaries <slaries@rambler.ru>
2020-08-27 15:04:49 +0300

    input user profile data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def update
+    current_user.update(user_params)
+    redirect_to edit_user_path(current_user)
+  end
+  def user_params
+  params.require(:user).permit(
+      :username, :name, :website, :bio, :email, :phone, :gender)
+  end
 end
